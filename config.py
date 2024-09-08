a5ecae7a5fa930b71091ee32e37d209563789cea
Arpan829810 <aar228491@gmail.com>
2024-09-08 18:50:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7025229260:AAHrBaKAOqaitTIspkVBu4Zw6gFVHFAYcck")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Arpanraj118")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1268121500")
 
 
 
